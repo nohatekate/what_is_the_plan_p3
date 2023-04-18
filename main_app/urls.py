978ea08eafaa2916a1905836bfcca80c28a520e3
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-18 17:44:21 -0500

    Group index view functionality and template inheritance (#2)

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('groups/', views.groups_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+groups = [
+    {'name': 'Old Greg Goes Camping'},
+    {'name': 'FamBam'},
+    {'name': 'Blue Bubble Commune'},
+    {'name': 'Most of My Friends'},
+    {'name': 'Booze and Sweatpants'},
+    {'name': 'The Shits'}
+]
+
 
 def home(request):
     return render(request, 'home.html')
+
+def groups_index(request):
+    # groups = Group.objects.filter(user=request.user)
+    return render(request, 'groups/index.html', {'groups': groups})

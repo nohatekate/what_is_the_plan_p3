708145418ef0a5119f4ed42967a97c9c5317f475
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-19 10:27:10 -0500

    Create admin (#5)
    * superuser created and import and register group model
    
    * redefine Group Model and create superuser

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import render
+from .models import Group
 
 # Create your views here.
-groups = [
-    {'name': 'Old Greg Goes Camping'},
-    {'name': 'FamBam'},
-    {'name': 'Blue Bubble Commune'},
-    {'name': 'Most of My Friends'},
-    {'name': 'Booze and Sweatpants'},
-    {'name': 'The Shits'}
-]
+# groups = [
+#     {'name': 'Old Greg Goes Camping'},
+#     {'name': 'FamBam'},
+#     {'name': 'Blue Bubble Commune'},
+#     {'name': 'Most of My Friends'},
+#     {'name': 'Booze and Sweatpants'},
+#     {'name': 'The Shits'}
+# ]
 
 
 def home(request):
     return render(request, 'home.html')
 
 def groups_index(request):
+    groups = Group.objects.all()
     # groups = Group.objects.filter(user=request.user)
     return render(request, 'groups/index.html', {'groups': groups})

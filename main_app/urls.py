62df3195ee16ec33f7a05a8a31030bb70a3babf3
nohatekate <kateiscoding@gmail.com>
2023-04-19 14:15:00 -0500

    update and delete routes defined

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('groups/', views.groups_index, name='index'),
     path('groups/<int:group_id>/', views.groups_detail, name='detail'),
     path('groups/create/', views.GroupCreate.as_view(), name='groups_create'),
+    path('groups/<int:pk>/update/', views.GroupUpdate.as_view(), name='groups_update'),
+    path('groups/<int:pk>/delete/', views.GroupDelete.as_view(), name='groups_delete'),
     path('accounts/signup/', views.signup, name='signup'),
 ]

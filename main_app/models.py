e017767a0b6a03a97e9a65b9b078c46f31144ebb
nohatekate <kateiscoding@gmail.com>
2023-04-19 10:23:55 -0500

    redefine Group Model and create superuser

--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Group(models.Model):
-    name = models.CharField(),
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # ideas = models.ForeignKey(Idea), 
-    # crew = models.ManyToManyField(user),
-    icon = models.ImageField(), 
+    name = models.CharField(max_length=100)
+    
 
-
+    def __str__(self):
+        return self.name

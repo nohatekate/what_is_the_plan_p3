9cdb5b1194417ecac09fb5e5d51ff3762610dcb7
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-19 14:00:41 -0500

    Authentication (#8)
    * update cat model, create user to group relationship, migrate changes, add urls
    
    * log in functionality
    
    * log out functionality
    
    * signup new user
    
    * updated creategroup form to specify name only in forms.py
    
    * view functions and class based views log-in protected

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import Group
+from .forms import CreateGroupForm
+
 
 # Create your views here.
 # groups = [
@@ -16,16 +23,36 @@ from .models import Group
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def groups_index(request):
     groups = Group.objects.all()
     # groups = Group.objects.filter(user=request.user)
     return render(request, 'groups/index.html', {'groups': groups})
 
+@login_required
 def groups_detail(request, group_id):
     group = Group.objects.get(id=group_id)
     return render(request, 'groups/detail.html', { 'group': group})
 
-class GroupCreate(CreateView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+class GroupCreate(LoginRequiredMixin, CreateView):
     model = Group
-    fields = '__all__'
-    success_url = '/groups'
+    fields = ['name']
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user  
+
+        return super().form_valid(form)

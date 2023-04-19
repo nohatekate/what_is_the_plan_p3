e74f6b25e63f06d725a4f3eacbfca423b7a61501
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-19 11:40:54 -0500

    new Group page view and functionality (#7)

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 # from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Group(models.Model):
@@ -8,3 +9,6 @@ class Group(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'group_id': self.id})

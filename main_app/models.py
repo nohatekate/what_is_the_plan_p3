7c9c88fbdcf4fb4d904a821f0daa4fa323314915
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-18 20:12:00 -0500

    create Group model (#4)

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+# from django.contrib.auth.models import User
 
 # Create your models here.
+class Group(models.Model):
+    name = models.CharField(),
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # ideas = models.ForeignKey(Idea), 
+    # crew = models.ManyToManyField(user),
+    icon = models.ImageField(), 
+
+

f42ff60a072b123746259872df4795e3e9663d3a
nohatekate <kateiscoding@gmail.com>
2023-04-19 11:06:29 -0500

    Add group details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('groups/', views.groups_index, name='index'),
+    path('groups/<int:group_id>/', views.groups_detail, name='detail'),
 ]

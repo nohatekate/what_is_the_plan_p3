8a655ebb7ce81604c082a9b6e636fec9d61b0594
nohatekate <kateiscoding@gmail.com>
2023-04-19 12:05:42 -0500

    log out functionality

--- what_is_the_plan/settings.py
+++ what_is_the_plan/settings.py
@@ -119,7 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-LOGIN_REDIRECT_URL = '/groups'
+LOGIN_REDIRECT_URL = '/groups/'
+
+LOGOUT_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

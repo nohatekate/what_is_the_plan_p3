7de4e2ee4a8a5b83b0c91406413b5baca11cf609
nohatekate <kateiscoding@gmail.com>
2023-04-20 18:00:09 -0500

    order groups alphabetically for ease of search

--- main_app/models.py
+++ main_app/models.py
@@ -8,11 +8,15 @@ class Group(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    
     def __str__(self):
         return self.name
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'group_id': self.id})
+    
+    class Meta:
+        ordering = ['name']
     
 class Idea(models.Model):
     name = models.CharField(max_length=100)

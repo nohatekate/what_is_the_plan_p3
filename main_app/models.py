2d084defec8a757547386fa721452c2686e3eeb5
nohatekate <119904831+nohatekate@users.noreply.github.com>
2023-04-21 15:30:46 -0500

    Morestretchgoals (#20)
    * check for match between user.id and requesting user before showing delete option
    
    * dropdown option added to environment field in idea form

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
+ENVIRONMENTS = (
+    ('A', 'Anywhere'),
+    ('I', 'Inside'),
+    ('O', 'Outside')
+)
 # Create your models here.
 class Group(models.Model):
     name = models.CharField(max_length=100)
@@ -23,7 +28,7 @@ class Idea(models.Model):
     estimated_time = models.IntegerField(default=0)
     estimated_cost = models.IntegerField(default=0)
     day_of_week = models.CharField(max_length=20, default=' ')
-    environment = models.CharField(max_length=100, default='inside or outside?')
+    environment = models.CharField(max_length=1, choices=ENVIRONMENTS, default=ENVIRONMENTS[0][0], blank=True)
 
     group = models.ForeignKey(Group, on_delete=models.CASCADE)
 
@@ -31,4 +36,4 @@ class Idea(models.Model):
         ordering = ['name']
         
     def __str__(self):
-        return self.name
+        return f"{self.name}"

3badc28658b366c2ff845901d92a49db4f90bd62
nohatekate <kateiscoding@gmail.com>
2023-04-23 11:04:29 -0500

    photo functionality complete, registered Photo model to admin

--- main_app/views.py
+++ main_app/views.py
@@ -60,16 +60,20 @@ def choose_random_idea(request, group_id):
 
 @login_required
 def add_photo(request, group_id):
+    
     # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
+        
         s3 = boto3.client('s3')
         # need a unique "key" for S3 / needs image file extension too
         key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
         # just in case something goes wrong
         try:
+            
             bucket = os.environ['S3_BUCKET']
             s3.upload_fileobj(photo_file, bucket, key)
+            print("add photo view function")
             # build the full url string
             url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
             # we can assign to cat_id or cat (if you have a cat object)
--- what_is_the_plan/settings.py
+++ what_is_the_plan/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import environ
 
 environ.Env()
-environ.Env.read_env 
+environ.Env.read_env() 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
